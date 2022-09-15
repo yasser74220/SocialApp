4a80511c766efcd4dcff3daab0258feb959b513b
yasser74220 <yasserahlawy970@gmail.com>
2022-09-15 16:40:18 +0200

    SocialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layouts/social_app/cubit/states.dart';
+import 'package:social_app/shared/components/components.dart';
 import 'package:social_app/shared/components/constant.dart';
 import 'package:social_app/shared/cubit/cubit.dart';
 import 'package:social_app/shared/cubit/observer.dart';
@@ -13,7 +15,12 @@ import 'package:social_app/shared/styles/themes.dart';
 import 'layouts/social_app/cubit/cubit.dart';
 import 'layouts/social_app/social_layout.dart';
 import 'modules/social_login/social_login_screen.dart';
-
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print(message.data.toString());
+  showToast(text: 'onmessage', state: ToastStates.SUCCESS);
+}
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -21,8 +28,20 @@ void main() async {
   dioHelper.init();
   await CacheHelper.init();
   Widget widget;
+  var token =await FirebaseMessaging.instance.getToken();
+  print(token);
+  FirebaseMessaging.onMessage.listen((event) {
+    print(event.data.toString());
+    showToast(text: 'onmessage', state: ToastStates.SUCCESS);
 
-  uId = CacheHelper.getData(key: 'uId');
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print(event.data.toString());
+    showToast(text: 'onmessageopened', state: ToastStates.SUCCESS);
+  });
+   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+   uId = CacheHelper.getData(key: 'uId');
 
   if(uId != null)
   {
